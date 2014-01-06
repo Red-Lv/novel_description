ef2a3b4c7eefb43a2bc637d2cad77abbb4ed4fb2
lvleibing01 <lvleibing01@baidu.com>
2014-01-06 17:14:12 +0800

    update

---
+++ halfwidth_fullwidth_transformation/README.md
@@ -0,0 +1,4 @@
+halfwidth_fullwidth_transformation
+==================================
+
+the library about transformation between full_width characters and half_width ones
---
+++ halfwidth_fullwidth_transformation/halfwidth_fullwidth_transformation.py
@@ -0,0 +1,45 @@
+#! /bin/env python
+#! -*coding:GBK-*-
+#! Usage:
+#!      transformation between the half width ascii characters and full width ones
+#!      for more detail, we should take the kana into consideration
+
+__author__ = 'lvleibing01'
+
+
+def halfwidth_to_fullwidth(uni_char):
+    """
+    """
+
+    if not isinstance(uni_char, unicode):
+        print 'the parameter uni_str is not a instance of unicode'
+        return None
+
+    if ord(uni_char) == 0x0020:
+        return unichr(0x3000)
+
+    if 0x0021 <= ord(uni_char) <= 0x007E:
+        return unichr(ord(uni_char) - 0x21 + 0xFF01)
+
+
+def fullwidth_to_halfwidth(uni_char):
+    """
+    """
+
+    if not isinstance(uni_char, unicode):
+        print 'the parameter uni_str is not a instance of unicode'
+        return None
+
+    if ord(uni_char) == 0x3000:
+        return unichr(0x0020)
+
+    if 0xFF01 <= ord(uni_char) <= 0xFF5E:
+        return unichr(ord(uni_char) - 0xFF01 + 0x21)
+
+if __name__ == '__main__':
+
+    a = '1'
+    print halfwidth_to_fullwidth(unicode(a))
+
+    b = '£¡'
+    print fullwidth_to_halfwidth(unicode(b, 'GBK'))
