b67534ce4cfad607ec2fe814aba8fc699bfe6ee4
lvleibing01 <lvleibing01@baidu.com>
2014-01-07 12:31:51 +0800

    update

--- book_name_pattern_replace.sh
+++ book_name_pattern_replace.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-data_dir=${1}
+book_name_pattern=${1}
+input_data_dir=${2}
+output_data_dir=${3}
 
 for((i = 0; i < 178; i ++))
 do
     site_id=${i}
     echo 'site_id: '${site_id}
-    python book_name_pen_name_pattern_replace.py ${site_id} >./${data_dir}/${site_id}.txt 2>./${data_dir}/${site_id}.txt.wf
+    python book_name_pen_name_pattern_replace.py ${book_name_pattern} ${site_id} ${input_data_dir} >./${output_data_dir}/${site_id}.txt 2>./${output_data_dir}/${site_id}.txt.wf
 done
