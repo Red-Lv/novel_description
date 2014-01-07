e53edb656e6fbe8b53791236a930c3eed82c311b
lvleibing01 <lvleibing01@baidu.com>
2014-01-07 12:45:03 +0800

    update

--- book_name_pattern_detect.sh
+++ book_name_pattern_detect.sh
@@ -7,5 +7,5 @@ for((i = 0; i < 178; i ++))
 do
     site_id=${i}
     echo 'site_id: '${site_id}
-    python book_name_pen_name_detect.py ./${data_dir}/${site_id}.txt
+    python book_name_pen_name_pattern_detect.py ./${data_dir}/${site_id}.txt
 done > ${pattern_file}
