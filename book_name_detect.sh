2a7ff41750efd4ab46228cdf49843848634deb67
lvleibing01 <lvleibing01@baidu.com>
2014-01-07 12:15:57 +0800

    update

--- book_name_detect.sh
+++ book_name_detect.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
+data_dir=${1}
+pattern_file=${2}
+
 for((i = 0; i < 178; i ++))
 do
     site_id=${i}
     echo 'site_id: '${site_id}
-    python book_name_pen_name_detect.py ./data_3/${site_id}.txt
-done
+    python book_name_pen_name_detect.py ./${data_dir}/${site_id}.txt
+done > ${pattern_file}
