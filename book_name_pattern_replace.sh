07ea051762b105a4f425eb91e76e04bb1e6d361d
lvleibing01 <lvleibing01@baidu.com>
2014-01-07 12:25:55 +0800

    update

---
+++ book_name_pattern_replace.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+data_dir=${1}
+
+for((i = 0; i < 178; i ++))
+do
+    site_id=${i}
+    echo 'site_id: '${site_id}
+    python book_name_pen_name_pattern_replace.py ${site_id} >./${data_dir}/${site_id}.txt 2>./${data_dir}/${site_id}.txt.wf
+done
