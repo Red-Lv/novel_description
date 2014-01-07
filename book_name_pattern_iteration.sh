ac8b06353fb38ec47c601127e5faf449472d5c07
lvleibing01 <lvleibing01@baidu.com>
2014-01-07 12:40:54 +0800

    update

--- book_name_pattern_iteration.sh
+++ book_name_pattern_iteration.sh
@@ -7,5 +7,7 @@ do
     sh book_name_pen_name_pattern_detect.sh ./data_${i}/ ./pattern/raw_book_name_pattern.txt.${i}
     sh filter_cs.py ./pattern/raw_book_name_pattern.txt.${i} > ./pattern/raw_book_name_pattern.txt.${i}.filtered
     python pattern_to_json.py ./pattern/raw_book_name_pattern.txt.${i}.filtered > ./pattern/book_name_pattern.json.${i}
-    sh book_name_pen_name_pattern_replace.sh ./pattern/book_name_pattern.json.${i}
+    ((next = i + 1))
+    mkdir -p ./data_${next}
+    sh book_name_pen_name_pattern_replace.sh ./pattern/book_name_pattern.json.${i} ./data_{i} ./data_${next}
 done
