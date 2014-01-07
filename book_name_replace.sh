1c532491e32c17b0ed0b4813626b015125619f3c
lvleibing01 <lvleibing01@baidu.com>
2014-01-07 12:16:41 +0800

    update

--- book_name_replace.sh
+++ book_name_replace.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+data_dir=${1}
+
 for((i = 0; i < 178; i ++))
 do
     site_id=${i}
     echo 'site_id: '${site_id}
-    python book_name_pen_name_replace.py ${site_id} >./data_3/${site_id}.txt 2>./data_3/${site_id}.txt.wf
+    python book_name_pen_name_replace.py ${site_id} >./${data_dir}/${site_id}.txt 2>./${data_dir}/${site_id}.txt.wf
 done
